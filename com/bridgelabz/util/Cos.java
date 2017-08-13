0aa4c47f0596568662dffecbce4ca3e94086a5ea
sujit <chincholkarsujit@gmail.com>
2017-08-13 13:07:18 +0530

    File added

--- com/bridgelabz/util/Cos.java
+++ com/bridgelabz/util/Cos.java
@@ -7,12 +7,13 @@
 ****************************************************************************/
 
 class Cos {
+   //Acepting input from command line
   static double findFactorial(int num) {
-   double fact=1;
-   for(double i=num;i>=1;i--) {
+    double fact=1;
+    for(double i=num;i>=1;i--) {
       fact=fact*i;
-  } 
-  return fact;
+    } 
+    return fact;
   }
   public static void main(String args[]) {
     double angle=Double.parseDouble(args[0]);
@@ -21,10 +22,10 @@ class Cos {
     double cosx=0,fact=1;
     while(count<5) {
       if(alt==0){
-      fact=findFactorial(power);
-      cosx=cosx+(Math.pow(angle,power)/fact);
-      power=power+2;
-      alt=1;
+       fact=findFactorial(power);
+       cosx=cosx+(Math.pow(angle,power)/fact);
+       power=power+2;
+       alt=1;
       } else {
            fact=findFactorial(power);
            cosx=cosx-(Math.pow(angle,power)/fact);
--- com/bridgelabz/util/Sin.java
+++ com/bridgelabz/util/Sin.java
@@ -7,24 +7,29 @@
 ****************************************************************************/
 
 class Sin {
+  //Function Calculating factorial
   static double findFactorial(int num) {
-   double fact=1;
-   for(double i=num;i>=1;i--) {
+    double fact=1;
+    for(double i=num;i>=1;i--) {
       fact=fact*i;
-  } 
-  return fact;
+    } 
+    return fact;
   }
+
   public static void main(String args[]) {
+    //Acepting input from command line
     double angle=Double.parseDouble(args[0]);
+     
     angle=angle%(2*Math.PI);
     int power=1,count=0,alt=0;
     double sinx=0,fact=1;
     while(count<5) {
+      //Checking Alternative sins 
       if(alt==0){
-      fact=findFactorial(power);
-      sinx=sinx+(Math.pow(angle,power)/fact);
-      power=power+2;
-      alt=1;
+        fact=findFactorial(power);
+        sinx=sinx+(Math.pow(angle,power)/fact);
+        power=power+2;
+        alt=1;
       } else {
            fact=findFactorial(power);
            sinx=sinx-(Math.pow(angle,power)/fact);
