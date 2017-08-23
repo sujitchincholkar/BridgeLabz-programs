5196c0a0bdceac8dbac83c98dc19f9ffe7b312fa
sujitchincholkar <30769970+sujitchincholkar@users.noreply.github.com>
2017-08-23 19:00:47 +0530

    File Added

---
+++ com/bridgelabz/util/PrintName.java
@@ -0,0 +1,10 @@
+class PrintName {
+  public static void main(String args[]) {
+    String name =args[0];
+    int length=name.length():
+    if(length>3) {
+      System.out.println("Hello "+name+",How are you);
+    } else {
+        System.out.println("Invalid name");
+    }
+  }
