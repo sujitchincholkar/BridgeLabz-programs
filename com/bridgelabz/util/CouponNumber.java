3c690a5916a1d5482c3c56e8d3f275681e786ce8
sujitchincholkar <30769970+sujitchincholkar@users.noreply.github.com>
2017-08-23 18:56:46 +0530

    Add files via upload

---
+++ com/bridgelabz/util/CouponNumber.java
@@ -0,0 +1,28 @@
+class CouponNumber {
+  public static void main(String args[]) {
+    int n=Integer.parseInt(args[0]);
+    int randomCount=1,randomNo,flag=0,index=1;
+    int a[]=new int[n];
+    a[0]= (int)(n*Math.random());
+    for(int i=0;index<n;i++)
+	  {
+			randomNo=(int)(Math.random()*n);
+      	randomCount++;
+      for(int j=0;j<index;j++){
+      	if(a[j]==randomNo){
+					flag=1; 	
+   			}
+    	}
+    	if(flag==0) {
+      	a[index]=randomNo;
+        index++;
+      	
+   		}
+		flag=0;
+	 }
+  System.out.println(randomCount);
+  for(int i:a){
+  	System.out.println(i);
+  }
+	}
+}
