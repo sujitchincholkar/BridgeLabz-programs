b52b13788b72f122b2ae9850ac38cb6da5c64cbd
sujitchincholkar <30769970+sujitchincholkar@users.noreply.github.com>
2017-08-26 18:04:38 +0530

    File Added

--- com/bridgelabz/programs/BubbleSort.java
+++ com/bridgelabz/programs/BubbleSort.java
@@ -10,14 +10,14 @@ public class BubbleSort {
 		int arr[]=new int[10];
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter array of 10 elements");
-        for(int i=0;i<10;i++){
-        	arr[i]=sc.nextInt();
-        }
-        Utility.bubbleSort(arr);
-        System.out.println("Sorted Array");
-        for(int i:arr){
-        	System.out.println(i);
-        }
+    for(int i=0;i<10;i++){
+  		arr[i]=sc.nextInt();
+    }
+    Utility.bubbleSort(arr);
+    System.out.println("Sorted Array");
+    for(int i:arr){
+     	System.out.println(i);
+    }
 	}
 
 }
